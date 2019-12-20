2e2fb92f360df62c6a26933d8ad2437f92bc2650
Ivette Dominguez <ivettedominguez104@gmail.com>
2019-12-20 15:26:34 +0000

    added new view called panda with associated route path

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -5,8 +5,9 @@ const getState = ({ getStore, getActions, setStore }) => {
 				{
 					title: "FIRST",
 					background: "white",
-					initial: "white"
-                    image: 'https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fhiddenremote.com%2Ffiles%2F2015%2F10%2Frobot-chicken.jpg&w=736&h=485&c=sc"
+					initial: "white",
+					image:
+						"https://imagesvc.timeincapp.com/v3/fan/image?url=https%3A%2F%2Fhiddenremote.com%2Ffiles%2F2015%2F10%2Frobot-chicken.jpg&w=736&h=485&c=sc"
 				},
 				{
 					title: "SECOND",
